b908faee0de9b1fe051958f7cdd4035084724418
BallThe <1539110121@qq.com>
2024-07-20 20:18:55 +0800

    finished return promises of function getChartData

--- Omega/play/play.js
+++ Omega/play/play.js
@@ -1,6 +1,6 @@
 function getUrlParams() {
     var vars = {};
-    var _parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, (_m,key,value) => {
+    var _parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, (_m, key, value) => {
         vars[key] = value;
     });
     return vars;
@@ -11,20 +11,44 @@ const CHARTS_ID = {
     "shadows": "Shadows"
 }
 
-function getChartFile(){
+function getChartData() {
     let urlParams = getUrlParams();
     let chartName = CHARTS_ID[urlParams["chart"]];
     let chartDifficulty = urlParams["difficulty"];
-    // Fetch获取谱面
-    fetch(`../charts/${chartName}/${chartDifficulty}.json`)
-    .then((_responseData)=>{
-        _responseData.json()
-        .then((chartData)=>{
-            console.log(chartData);
-        });
-    });
+
+    // 返回获取chartData的Promise函数
+    return new Promise((resolve, reject) => {
+
+        // Fetch获取谱面
+        fetch(`../charts/${chartName}/${chartDifficulty}.json`)
+            // 返回后获取
+            .then(_responseData => {
+                // 因网络原因获取失败
+                if(!_responseData.ok){
+                    reject(new Error("Failed to Fetch chartData Because of Network Problems"));
+                }
+                // 将获取的值在json解析时返回
+                return _responseData.json();
+            })
+            // 返回json解析后的chartData
+            .then(chartData => {
+                resolve(chartData);
+            })
+            .catch(err => {
+                reject(err);
+            });
+    })
+}
+
+function loadChart() {
+    let timeStartGettingChartData = new Date().getTime();
+    getChartData().then(chartData => {
+        let timeFinishedGettingChartData = new Date().getTime();
+        console.log("获取谱面信息成功，用时",timeFinishedGettingChartData-timeStartGettingChartData,"ms");
+        console.log(chartData);
+    })
 }
 
 window.onload = () => {
-    getChartFile();
+    loadChart();
 }
